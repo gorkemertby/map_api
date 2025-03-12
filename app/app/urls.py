0bbc110b6bdf4b7b944fdb7f4691c508b435fe76
Gorkem Mert <gorkemert.by@gmail.com>
2025-03-12 14:20:09 +0300

    AllTurkeyNeighboursData

---
+++ app/app/urls.py
@@ -0,0 +1,23 @@
+import requests
+
+url = "https://turkiyeapi.dev/api/v1/neighborhoods"
+limit = 100
+offset = 0 
+neighborhoods = []
+
+while True:
+    response = requests.get(url, params={'limit': limit, 'offset': offset})
+    
+    if response.status_code == 200:
+        data = response.json()
+        neighborhoods.extend(data['data'])
+        print(data)
+        if len(data['data']) < limit:
+            break
+        else:
+            offset += limit
+    else:
+        print(f"Veri alınamadı: {response.status_code}")
+        break
+
+print(f"Toplam {len(neighborhoods)} mahalle çekildi.")
